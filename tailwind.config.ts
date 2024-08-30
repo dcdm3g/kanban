92d9dbede7000cf78bfc2f9892564965cdf54605
Davi Melo <davicosta01082008@gmail.com>
2024-08-30 20:04:59 -0300

    chore: configure tools

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  content: ['src/app/**/*.tsx', 'src/components/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
