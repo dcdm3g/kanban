7d591910cf89ab5c70d29c06a00c303cacc1771a
Davi Melo <davicosta01082008@gmail.com>
2024-09-19 13:44:45 -0300

    fix: remove unnecessary `signInWithOtp` options
    The options passed to the Supabase `signInWithOtp` function are
    unnecessary. The `emailRedirectTo` option is not used and the
    `shouldCreateUser` option set to `false` prevents users from signing up
    via OTPs, which is not the expected behavior.
    
    This commit just removes these options.

--- apps/www/src/actions/login-with-email.ts
+++ apps/www/src/actions/login-with-email.ts
@@ -8,16 +8,7 @@ export const loginWithEmail = createServerAction()
 	.input(z.object({ email: z.string().email() }))
 	.handler(async ({ input: { email } }) => {
 		const supabase = createClient()
-
-		const { error } = await supabase.auth.signInWithOtp({
-			email,
-			options: {
-				emailRedirectTo: process.env.VERCEL_URL
-					? 'https://' + process.env.VERCEL_URL
-					: 'http://localhost:3000',
-				shouldCreateUser: false,
-			},
-		})
+		const { error } = await supabase.auth.signInWithOtp({ email })
 
 		if (error) {
 			throw 'Something went wrong. Please try again later.'
